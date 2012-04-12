6df33ef35f935af5bed9ca8a277d04ceb5773be6
Brandon Sterne <brandon.sterne@gmail.com>
2012-04-12 10:22:28 -0700

    changed default charset to utf8

--- data/table.sql
+++ data/table.sql
@@ -29,7 +29,7 @@ CREATE TABLE `alerts` (
   `source` varchar(512) DEFAULT NULL,
   `date_sent` datetime DEFAULT NULL,
   PRIMARY KEY (`aid`)
-) ENGINE=MyISAM AUTO_INCREMENT=1877 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=1877 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
